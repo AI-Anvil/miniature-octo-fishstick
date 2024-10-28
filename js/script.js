6e463a6c621038f21e4b4f50540e44dc01ade2ad
Wassim Askoul <askoulwassim@gmail.com>
2024-10-28 16:35:53 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// JavaScript for Rolling Gallery
+// Adjust the speed of the gallery by modifying the animation duration in CSS
+
+// Optionally, you can implement a more advanced carousel using JavaScript
+
+// Placeholder for future JavaScript code
